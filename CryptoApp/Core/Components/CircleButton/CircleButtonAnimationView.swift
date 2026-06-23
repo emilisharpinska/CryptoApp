54b9d51388ab5715e3913eacf6f3d0cf0e2be797
Emili Sharpinska <emili.sharpinsk@gmail.com>
2026-06-23 15:28:32 +0200

    [Feature] Create animation for buttons

---
+++ CryptoApp/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,28 @@
+//
+//  CircleButtonAnimationView.swift
+//  CryptoApp
+//
+//  Created by Emili Sharpinska on 23/06/2026.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    
+    @Binding var animate: Bool
+    
+    var body: some View {
+        Circle()
+            .stroke(lineWidth: 5.0)
+            .scale(animate ? 1.0 : 0.0)
+            .opacity(animate ? 0.0 : 1.0)
+            .animation(animate ? Animation.easeOut(duration: 1.0) : .none, value: animate)
+            
+    }
+}
+
+#Preview {
+    CircleButtonAnimationView(animate: .constant(false))
+        .foregroundStyle(.red)
+        .frame(width: 100, height: 100)
+}
