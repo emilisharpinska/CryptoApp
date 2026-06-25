47909d6b456a9c7471b8f960efbaa47ba4bb1444
Emili Sharpinska <emili.sharpinsk@gmail.com>
2026-06-25 07:48:36 +0200

    [Feature] Create a coin in extension for all previews

---
+++ CryptoApp/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinRowView.swift
+//  CryptoApp
+//
+//  Created by Emili Sharpinska on 24/06/2026.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
