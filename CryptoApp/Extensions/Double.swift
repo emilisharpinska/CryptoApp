5cfc7ad733e39ed4c838d8ed27f77421c9467317
Emili Sharpinska <emili.sharpinsk@gmail.com>
2026-06-25 14:48:56 +0200

    [Feature] Add few func for formattiing of Double

--- CryptoApp/Extensions/Double.swift
+++ CryptoApp/Extensions/Double.swift
@@ -8,6 +8,32 @@
 import Foundation
 
 extension Double {
+    /// Converte a Double into a Currency with 2 decimal places
+    /// ```
+    /// Converte 1234.56 to $1,234.56
+    /// ```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        //add comas to numbers
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = .current // <- default value
+        formatter.currencyCode = "usd" // <- change currency
+        formatter.currencySymbol = "$" // <- change currency symbol
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    /// Converte a Double into a Currency as a String with 2 decimal places
+    /// ```
+    /// Converte 1234.56 to "$1,234.56"
+    /// ```
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "$0.00"
+    }
+    
     /// Converte a Double into a Currency with 2-6 decimal places
     /// ```
     /// Converte 1234.56 to $1,234.56
@@ -19,9 +45,9 @@ extension Double {
         //add comas to numbers
         formatter.usesGroupingSeparator = true
         formatter.numberStyle = .currency
-        //        formatter.locale = .current // <- default value
-        //        formatter.currencyCode = "usd" // <- change currency
-        //        formatter.currencySymbol = "$" // <- change currency symbol
+        formatter.locale = .current // <- default value
+        formatter.currencyCode = "usd" // <- change currency
+        formatter.currencySymbol = "$" // <- change currency symbol
         formatter.minimumFractionDigits = 2
         formatter.maximumFractionDigits = 6
         return formatter
@@ -36,5 +62,21 @@ extension Double {
     func asCurrencyWith6Decimals() -> String {
         let number = NSNumber(value: self)
         return currencyFormatter6.string(from: number) ?? "$0.00"
+    }
+    
+    /// Converte a Double into a string representation
+    /// ```
+    /// Converte 1.23456 to "1.23"
+    /// ```
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    /// Converte a Double into a string representation with percent symbol
+    /// ```
+    /// Converte 1.23456 to "1.23%"
+    /// ```
+    func asPercentString() -> String {
+        return asNumberString() + "%"
     }
 }
