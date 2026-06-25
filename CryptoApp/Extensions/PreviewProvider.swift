b002325caec6a8c32201155c36ab39451bf415e9
Emili Sharpinska <emili.sharpinsk@gmail.com>
2026-06-25 07:55:53 +0200

    [Fix] Delete extension for oldest version of preview

--- CryptoApp/Extensions/PreviewProvider.swift
+++ CryptoApp/Extensions/PreviewProvider.swift
@@ -8,12 +8,6 @@
 import Foundation
 import SwiftUI
 
-extension PreviewProvider {
-    static var dev: DeveloperPreview {
-        return DeveloperPreview.instance
-    }
-}
-
 class DeveloperPreview {
     static let instance = DeveloperPreview()
     private init(){}
