8f84156986afb71681cbbd24ada9fa2f2150b3ab
Emili Sharpinska <emili.sharpinsk@gmail.com>
2026-07-09 21:51:46 +0200

    [Future] Create separate view for images of coins with  connected viewModel

--- CryptoApp/Core/Components/CoinImage/CoinImageView.swift
+++ CryptoApp/Core/Components/CoinImage/CoinImageView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel
+    
+    init(coin: CoinModel){
+        _vm = StateObject(wrappedValue: CoinImageViewModel(coin: coin))
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            } else if vm.isLoading {
+                ProgressView()
+            } else {
+                Image(systemName: "questionmark")
+                    .foregroundStyle(Color.theme.secondaryText)
+            }
+        }
     }
 }
 
 #Preview {
-    CoinImageView()
+    CoinImageView(coin: DeveloperPreview.instance.coin)
 }
