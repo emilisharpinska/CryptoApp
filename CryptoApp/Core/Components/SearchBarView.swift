7a1be65804b9f3540ed21cc45bb0ba544acd7d9f
Emili Sharpinska <emili.sharpinsk@gmail.com>
2026-07-18 10:24:26 +0200

    [Feature] Create search bar for main screen which is reusable through all app

---
+++ CryptoApp/Core/Components/SearchBarView.swift
@@ -0,0 +1,50 @@
+//
+//  SearchBarView.swift
+//  CryptoApp
+//
+//  Created by Emili Sharpinska on 18/07/2026.
+//
+
+import SwiftUI
+
+struct SearchBarView: View {
+    
+    @Binding var searchText: String
+    
+    var body: some View {
+        HStack{
+            Image(systemName: "magnifyingglass")
+                .foregroundStyle(
+                    searchText.isEmpty ? Color.theme.secondaryText : Color.theme.accent
+                )
+             
+            TextField("Search by name of symbol...", text: $searchText)
+                .foregroundColor(Color.theme.accent)
+                .disableAutocorrection(true)
+                .overlay(
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 10)
+                        .foregroundStyle(Color.theme.accent)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                        .onTapGesture {
+                            UIApplication.shared.endEditing()
+                            searchText = ""
+                        }
+                    , alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.theme.background)
+                .shadow(color: Color.theme.accent.opacity(0.15), radius: 10, x: 0)
+        )
+        .padding()
+    }
+}
+
+#Preview {
+    SearchBarView(searchText: .constant(""))
+}
